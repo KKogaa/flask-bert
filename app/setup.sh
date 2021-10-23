7b94f71d45ab53e8d3899a22cc6223f466c52d96
KKogaa <a20160037@pucp.edu.pe>
2021-10-23 10:09:19 -0400

    bash thing

---
+++ app/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install mysql-client
+pip3 install torch
+pip3 install pytorch-lightning
+pip3 install flask
+pip3 install flask-restx
+pip3 install tensorflow-text
+pip3 install tensorflow-hub
+pip3 install flask-sqlalchemy
+pip3 install waitress
